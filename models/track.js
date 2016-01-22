7ea61a8eda95f63b7280f0e8809f1a1d116f240e
Berto_ABC <vermibeto@gmail.com>
2016-01-22 03:59:21 +0100

    smal fixes

--- models/track.js
+++ models/track.js
@@ -19,23 +19,5 @@ var trackSchema = new Schema({
   url: String
 });
 
-/*exports.tracks = {
-	1: {
-		name: 'Cute',
-		url: '/media/Cute.mp3'
-	},
-	2: {
-		name: 'Dubstep',
-		url: '/media/Dubstep.mp3'
-	},
-	3: {
-		name: 'Epic',
-		url: '/media/Epic.mp3'
-	},
-	4: {
-		name: 'Littleidea',
-		url: '/media/Littleidea.mp3'
-	}
-};*/
 
 var Track = mongoose.model('Track', trackSchema);
